87ad146f006e83b3617265586dfaf8b5564a38b5
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-16 15:12:34 -0300

    first commit

---
+++ src/main/java/tech/ada/java/agendamentoconsultas/AgendamentoConsultasApplication.java
@@ -0,0 +1,13 @@
+package tech.ada.java.agendamentoconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoConsultasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendamentoConsultasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/ada/java/agendamentoconsultas/AgendamentoConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.java.agendamentoconsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
