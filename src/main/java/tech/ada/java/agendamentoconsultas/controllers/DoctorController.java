fb622e692516bbb7ad986d1b7ced5b1bbd11e3aa
Caio <caiolyra1@hotmail.com>
2024-04-20 18:19:46 -0300

    feat : implementar funcionalidade de buscar por Uuid;

--- src/main/java/tech/ada/java/agendamentoconsultas/controllers/DoctorController.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/controllers/DoctorController.java
@@ -2,12 +2,14 @@ package tech.ada.java.agendamentoconsultas.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.ada.java.agendamentoconsultas.model.Doctor;
 import tech.ada.java.agendamentoconsultas.service.DoctorService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/doctors")
@@ -22,6 +24,11 @@ public class DoctorController {
     public List<Doctor> findAll(){
 
         return this.service.findALl();
+    }
+
+    @GetMapping("/{uuid}")
+    public Doctor findByUuid(@PathVariable UUID uuid){
+        return service.findByUuid(uuid);
     }
 
 }
