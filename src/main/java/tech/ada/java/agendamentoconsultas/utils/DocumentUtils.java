2fa4ef7f68123e4cb69a97c202530f81f9541d16
dcbarros <dcbarros1@gmail.com>
2024-04-20 17:11:49 -0300

    feat(dcbarros): Adiciona validador de cpf na classe de documentUtils.

--- src/main/java/tech/ada/java/agendamentoconsultas/utils/DocumentUtils.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/utils/DocumentUtils.java
@@ -1,5 +1,28 @@
 package tech.ada.java.agendamentoconsultas.utils;
 
 public class DocumentUtils {
-    
+    public static Boolean cpfIsValid(String cpf){
+        String document = cpf;
+        cpf = document.replaceAll("[^0-9]", "");
+
+        if(cpf.length() != 11) return false;
+        if(cpf.matches("(\\d)\\1*")) return false;
+
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            sum += (10 - i) * (cpf.charAt(i) - '0');
+        }
+        int digit1 = 11 - (sum % 11);
+        if (digit1 > 9) digit1 = 0;
+
+        sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += (11 - i) * (cpf.charAt(i) - '0');
+        }
+        int digit2 = 11 - (sum % 11);
+        if (digit2 > 9) digit2 = 0;
+
+        return (cpf.charAt(9) - '0') == digit1 && (cpf.charAt(10) - '0') == digit2;
+    }
 }
