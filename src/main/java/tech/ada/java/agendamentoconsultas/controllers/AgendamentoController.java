15b24e90baeaf2eb142e03e0e3ef1ae458a993d2
unknown <caiolyra1@hotmail.com>
2024-04-20 14:07:09 -0300

    feat : Implementar entidade de medicos

--- src/main/java/tech/ada/java/agendamentoconsultas/controllers/AgendamentoController.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/controllers/AgendamentoController.java
@@ -12,3 +12,4 @@ public class AgendamentoController {
         return "Agendamento consulta";
     }
 }
+
---
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Doctor.java
@@ -0,0 +1,31 @@
+package tech.ada.java.agendamentoconsultas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer crm;
+
+    private Boolean isActive;
+
+
+
+}
