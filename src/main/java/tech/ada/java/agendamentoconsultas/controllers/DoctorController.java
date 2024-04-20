668702056b0fa69b1a10ddec20c5abfdbe48177c
Caio <caiolyra1@hotmail.com>
2024-04-20 17:11:02 -0300

    feat : criar rota get para medicos

---
+++ src/main/java/tech/ada/java/agendamentoconsultas/controllers/DoctorController.java
@@ -0,0 +1,27 @@
+package tech.ada.java.agendamentoconsultas.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.ada.java.agendamentoconsultas.model.Doctor;
+import tech.ada.java.agendamentoconsultas.service.DoctorService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doctors")
+@RequiredArgsConstructor
+
+public class DoctorController {
+
+    private DoctorService service;
+
+    @GetMapping
+
+    public List<Doctor> findAll(){
+
+        return this.service.findALl();
+    }
+
+}
