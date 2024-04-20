ea96d8736332c2e614a7a13b48b914bb40f728a3
Caio <caiolyra1@hotmail.com>
2024-04-20 17:11:52 -0300

    feat : criar repositorio para medicos

---
+++ src/main/java/tech/ada/java/agendamentoconsultas/service/DoctorService.java
@@ -0,0 +1,21 @@
+package tech.ada.java.agendamentoconsultas.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import tech.ada.java.agendamentoconsultas.model.Doctor;
+import tech.ada.java.agendamentoconsultas.respository.DoctorRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
+public class DoctorService {
+    private DoctorRepository repository;
+    public List<Doctor> findALl() {
+
+        return this.repository.findAll();
+    };
+
+}
