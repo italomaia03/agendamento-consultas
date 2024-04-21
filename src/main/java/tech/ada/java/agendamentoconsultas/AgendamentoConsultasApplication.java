4e92414df70f7f7c4e1eb24fb9eac847dc039d98
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-21 10:39:11 -0300

    feat: adicionar bean de mapeamento de DTOs para entidades

--- src/main/java/tech/ada/java/agendamentoconsultas/AgendamentoConsultasApplication.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/AgendamentoConsultasApplication.java
@@ -1,7 +1,9 @@
 package tech.ada.java.agendamentoconsultas;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgendamentoConsultasApplication {
@@ -10,4 +12,8 @@ public class AgendamentoConsultasApplication {
         SpringApplication.run(AgendamentoConsultasApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
