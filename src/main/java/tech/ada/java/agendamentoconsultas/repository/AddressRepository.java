618152258d6e882786414ae4c0e004d2b19b1af1
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-22 11:01:40 -0300

    feat: adicionar busca por CEP e número do endereço

--- src/main/java/tech/ada/java/agendamentoconsultas/repository/AddressRepository.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/repository/AddressRepository.java
@@ -1,9 +1,12 @@
 package tech.ada.java.agendamentoconsultas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import tech.ada.java.agendamentoconsultas.model.Address;
 
+import java.util.Optional;
+
+@Repository
 public interface AddressRepository extends JpaRepository<Address,Long>{
-    
+    Optional<Address> findByCepAndNumero(String cep, Integer numero);
 }
