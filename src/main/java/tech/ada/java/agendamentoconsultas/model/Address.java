9ffd5c28d18e628141369ea0c7f4a7d348e5be2b
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-22 10:36:03 -0300

    feat: adicionar atributo de número

--- src/main/java/tech/ada/java/agendamentoconsultas/model/Address.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Address.java
@@ -16,6 +16,7 @@ public class Address {
     private String cep;
     private String logradouro;
     private String complemento;
+    private Integer numero;
     private String bairro;
     private String localidade;
     private String uf;
@@ -24,11 +25,12 @@ public class Address {
     private String ddd;
     private String siafi;
 
-    public Address(String cep, String logradouro, String complemento, String bairro, String localidade, String uf,
-            String ibge, String gia, String ddd, String siafi) {
+    public Address(String cep, String logradouro, String complemento, Integer numero, String bairro, String localidade, String uf,
+                   String ibge, String gia, String ddd, String siafi) {
         this.cep = cep;
         this.logradouro = logradouro;
         this.complemento = complemento;
+        this.numero = numero;
         this.bairro = bairro;
         this.localidade = localidade;
         this.uf = uf;
@@ -64,6 +66,8 @@ public class Address {
     public void setComplemento(String complemento) {
         this.complemento = complemento;
     }
+    public Integer getNumero() {return numero;}
+    public void setNumero(Integer numero) {this.numero = numero;}
     public String getBairro() {
         return bairro;
     }
