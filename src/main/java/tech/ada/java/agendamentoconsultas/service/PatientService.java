a8d8a3e9462ab536fdaa3a2e277440ffd0addabb
Pedro Medeiros das Chagas <pmdcpe@gmail.com>
2024-04-23 17:58:33 -0300

    feat: adiciona regra de mudança de senha ao PatientService.

--- src/main/java/tech/ada/java/agendamentoconsultas/service/PatientService.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/service/PatientService.java
@@ -4,9 +4,11 @@ import java.util.UUID;
 
 import tech.ada.java.agendamentoconsultas.model.Dto.PatientDtoRequest;
 import tech.ada.java.agendamentoconsultas.model.Dto.PatientDtoResponse;
+import tech.ada.java.agendamentoconsultas.model.Dto.PatientUpdatePasswordDto;
 import tech.ada.java.agendamentoconsultas.model.Dto.PatientUpdateRequestDto;
 
 public interface PatientService {
     PatientDtoResponse createPatient(PatientDtoRequest request);
     void update(PatientUpdateRequestDto request, UUID uuid);
+    void changePassword(PatientUpdatePasswordDto request, UUID uuid);
 }
