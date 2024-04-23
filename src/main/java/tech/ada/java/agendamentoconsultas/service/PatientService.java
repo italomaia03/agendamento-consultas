4ec909a5ae52a2a8c0c739fa075c5abe6ac64d14
Pedro Medeiros das Chagas <pmdcpe@gmail.com>
2024-04-23 18:29:58 -0300

    feat: cria regra de negocio para método de exibição.

--- src/main/java/tech/ada/java/agendamentoconsultas/service/PatientService.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/service/PatientService.java
@@ -1,14 +1,14 @@
 package tech.ada.java.agendamentoconsultas.service;
 
+import java.util.List;
 import java.util.UUID;
 
-import tech.ada.java.agendamentoconsultas.model.Dto.PatientDtoRequest;
-import tech.ada.java.agendamentoconsultas.model.Dto.PatientDtoResponse;
-import tech.ada.java.agendamentoconsultas.model.Dto.PatientUpdatePasswordDto;
-import tech.ada.java.agendamentoconsultas.model.Dto.PatientUpdateRequestDto;
+import tech.ada.java.agendamentoconsultas.model.Dto.*;
 
 public interface PatientService {
     PatientDtoResponse createPatient(PatientDtoRequest request);
     void update(PatientUpdateRequestDto request, UUID uuid);
     void changePassword(PatientUpdatePasswordDto request, UUID uuid);
+
+    List<PatientGetAllResponseDto> getAll();
 }
