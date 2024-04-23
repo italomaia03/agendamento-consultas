fe1a0cddad594cd4ec72a5a49f4a232cc8080a99
dcbarros <dcbarros1@gmail.com>
2024-04-22 22:56:07 -0300

    feat: Adiciona método de busca pelo UUID no repositorio do paciente.

--- src/main/java/tech/ada/java/agendamentoconsultas/repository/PatientRepository.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/repository/PatientRepository.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Repository;
 import tech.ada.java.agendamentoconsultas.model.Patient;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
    Optional<Patient> findByEmail(String email);
+   Optional<Patient> findByUuid(UUID uuid);
 }
