061f8fbb3d3df04cb634cb7abd6f7084430d6ccf
Pedro Medeiros das Chagas <pmdcpe@gmail.com>
2024-04-22 21:45:24 -0300

    feat: Cria interface para geração da atualização do cadsastro.

--- src/main/java/tech/ada/java/agendamentoconsultas/service/PatientService.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/service/PatientService.java
@@ -2,7 +2,9 @@ package tech.ada.java.agendamentoconsultas.service;
 
 import tech.ada.java.agendamentoconsultas.model.Dto.PatientDtoRequest;
 import tech.ada.java.agendamentoconsultas.model.Dto.PatientDtoResponse;
+import tech.ada.java.agendamentoconsultas.model.Dto.PatientUpdateRequestDto;
 
 public interface PatientService {
     PatientDtoResponse createPatient(PatientDtoRequest request);
+    void update(PatientUpdateRequestDto request, String email);
 }
