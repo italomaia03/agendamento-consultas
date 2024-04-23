5f7558ab5b56f13c2be29d43fd859fb7840ef27b
dcbarros <dcbarros1@gmail.com>
2024-04-22 22:55:20 -0300

    raw: modifica campo do DTO Response do paciente para UUID ao invés de CPF

--- src/main/java/tech/ada/java/agendamentoconsultas/model/Dto/PatientDtoResponse.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Dto/PatientDtoResponse.java
@@ -1,15 +1,16 @@
 package tech.ada.java.agendamentoconsultas.model.Dto;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 public class PatientDtoResponse implements Serializable{
     private String nome;
     private String email;
-    private String cpf;
-    public PatientDtoResponse(String nome, String email, String cpf) {
+    private UUID uuid;
+    public PatientDtoResponse(String nome, String email, UUID uuid) {
         this.nome = nome;
         this.email = email;
-        this.cpf = cpf;
+        this.uuid = uuid;
     }
     public PatientDtoResponse() {
     }
@@ -25,11 +26,11 @@ public class PatientDtoResponse implements Serializable{
     public void setEmail(String email) {
         this.email = email;
     }
-    public String getCpf() {
-        return cpf;
+    public UUID getUuid() {
+        return uuid;
     }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
     }
 
     
