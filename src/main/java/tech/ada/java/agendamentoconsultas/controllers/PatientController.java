3c2571c7a0817cfc1f2949e5b2bf183757c2d69d
Pedro Medeiros das Chagas <pmdcpe@gmail.com>
2024-04-23 17:54:55 -0300

    feat: integra método de atualização do service ao controller.

--- src/main/java/tech/ada/java/agendamentoconsultas/controllers/PatientController.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/controllers/PatientController.java
@@ -1,17 +1,15 @@
 package tech.ada.java.agendamentoconsultas.controllers;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
-
 import org.springframework.http.HttpStatus;
-
-
+import org.springframework.web.bind.annotation.*;
 import tech.ada.java.agendamentoconsultas.model.Dto.PatientDtoRequest;
 import tech.ada.java.agendamentoconsultas.model.Dto.PatientDtoResponse;
+import tech.ada.java.agendamentoconsultas.model.Dto.PatientUpdatePasswordDto;
 import tech.ada.java.agendamentoconsultas.model.Dto.PatientUpdateRequestDto;
 import tech.ada.java.agendamentoconsultas.service.PatientService;
+
+import java.util.UUID;
 
 
 @RestController
@@ -26,13 +24,19 @@ public class PatientController {
 
     @PostMapping("/sing-in")
     @ResponseStatus(HttpStatus.CREATED)
-    public PatientDtoResponse createPatient(@RequestBody @Valid PatientDtoRequest request){
+    public PatientDtoResponse createPatient(@RequestBody @Valid PatientDtoRequest request) {
         return patientService.createPatient(request);
     }
 
     @PutMapping("/{uuid}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@RequestBody @Valid PatientUpdateRequestDto request, @PathVariable UUID uuid){
+    public void update(@RequestBody @Valid PatientUpdateRequestDto request, @PathVariable UUID uuid) {
         patientService.update(request, uuid);
+    }
+
+    @PatchMapping("/{uuid}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changePassword(@RequestBody @Valid PatientUpdatePasswordDto request, @PathVariable UUID uuid) {
+        patientService.changePassword(request, uuid);
     }
 }
