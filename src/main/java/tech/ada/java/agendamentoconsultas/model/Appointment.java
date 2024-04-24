83bd1e7ae5c3b268a61f59fcbede7ea172996e94
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-24 12:49:26 -0300

    feat: implementar entidade para Appointments

---
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Appointment.java
@@ -0,0 +1,31 @@
+package tech.ada.java.agendamentoconsultas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentStartTime;
+    private LocalTime appointmentEndTime;
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+}
