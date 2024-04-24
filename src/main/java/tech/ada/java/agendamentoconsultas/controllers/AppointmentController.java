24b3a49e7add2135237f7f47e33fb99db04fc6e9
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-24 12:52:29 -0300

    feat: implementar rota POST para criação de novos appointments

---
+++ src/main/java/tech/ada/java/agendamentoconsultas/controllers/AppointmentController.java
@@ -0,0 +1,24 @@
+package tech.ada.java.agendamentoconsultas.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tech.ada.java.agendamentoconsultas.model.Dto.AppointmentRequestDto;
+import tech.ada.java.agendamentoconsultas.model.Dto.AppointmentResponseDto;
+import tech.ada.java.agendamentoconsultas.service.AppointmentService;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/patients")
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @PostMapping("/{patientUuid}/doctors/{doctorUuid}")
+    public AppointmentResponseDto create(@RequestBody @Valid AppointmentRequestDto request, @PathVariable UUID doctorUuid, @PathVariable UUID patientUuid) {
+        return appointmentService.create(request, doctorUuid, patientUuid);
+    }
+
+}
