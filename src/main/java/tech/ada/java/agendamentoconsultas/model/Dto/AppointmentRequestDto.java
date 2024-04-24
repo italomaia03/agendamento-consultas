f78fb8e5d8c7bbbf00f6a12b72d3bd69ee51a3cf
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-24 12:48:04 -0300

    feat: implementar DTO para requests de agendamento de consulta

---
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Dto/AppointmentRequestDto.java
@@ -0,0 +1,25 @@
+package tech.ada.java.agendamentoconsultas.model.Dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppointmentRequestDto implements Serializable {
+    @NotNull
+    @FutureOrPresent(message = "Informe uma data válida a partir da data atual")
+    private LocalDate appointmentDate;
+    @NotNull
+    @FutureOrPresent(message = "Informe um horário válido")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    private LocalTime appointmentStartTime;
+}
