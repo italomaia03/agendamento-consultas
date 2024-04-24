260f221052ac1cc5afc4874bf417bb77ff8dd285
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-24 12:48:44 -0300

    feat: implementar DTO para response de para doctor ao realizar agendamento de consulta

---
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Dto/DoctorAppointmentResponseDto.java
@@ -0,0 +1,16 @@
+package tech.ada.java.agendamentoconsultas.model.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorAppointmentResponseDto implements Serializable {
+    private String name;
+    private String crm;
+    private String specialty;
+}
