eb0afcae48402e0a900ec103295045a247a5d04a
Caio <caiolyra1@hotmail.com>
2024-04-24 15:22:28 -0300

    feat : criar rota de consultas marcadas para determinado paciente

--- src/main/java/tech/ada/java/agendamentoconsultas/controllers/AppointmentController.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/controllers/AppointmentController.java
@@ -7,6 +7,7 @@ import tech.ada.java.agendamentoconsultas.model.Dto.AppointmentRequestDto;
 import tech.ada.java.agendamentoconsultas.model.Dto.AppointmentResponseDto;
 import tech.ada.java.agendamentoconsultas.service.AppointmentService;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -15,6 +16,11 @@ import java.util.UUID;
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
+
+    @GetMapping ("/{patientUuid}")
+    public List<AppointmentResponseDto> findAllByPatientUuid(@PathVariable UUID patientUuid) {
+        return appointmentService.findAllByPatientUuid(patientUuid);
+    }
 
     @PostMapping("/{patientUuid}/doctors/{doctorUuid}")
     public AppointmentResponseDto create(@RequestBody @Valid AppointmentRequestDto request, @PathVariable UUID doctorUuid, @PathVariable UUID patientUuid) {
