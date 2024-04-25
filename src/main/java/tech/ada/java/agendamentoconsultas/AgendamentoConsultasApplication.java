0d7c3ad50bd16f45e7157ba24f54ee9b5a8da81d
dcbarros <dcbarros1@gmail.com>
2024-04-25 17:54:42 -0300

    refactor: Mudança das configurações do swagger

--- src/main/java/tech/ada/java/agendamentoconsultas/AgendamentoConsultasApplication.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/AgendamentoConsultasApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
@@ -18,5 +24,24 @@ public class AgendamentoConsultasApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        final String apiTitle = "To-Do List API";
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(
+                new Components()
+                    .addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            )
+            .info(new Info().title(apiTitle).version("0.0.1-SNAPSHOT"));
     }
 }
--- src/main/java/tech/ada/java/agendamentoconsultas/security/service/AuthorizationService.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/security/service/AuthorizationService.java
@@ -1,16 +1,10 @@
 package tech.ada.java.agendamentoconsultas.security.service;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import tech.ada.java.agendamentoconsultas.repository.PatientRepository;
 
@@ -23,24 +17,5 @@ public class AuthorizationService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return patientRepository.findByEmail(username);
-    }
-    
-    @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "basicAuth";
-        final String apiTitle = "To-Do List API";
-        return new OpenAPI()
-            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-            .components(
-                new Components()
-                    .addSecuritySchemes(securitySchemeName,
-                        new SecurityScheme()
-                            .name(securitySchemeName)
-                            .type(SecurityScheme.Type.HTTP)
-                            .scheme("basic")
-//                            .bearerFormat("JWT")
-                    )
-            )
-            .info(new Info().title(apiTitle).version("0.0.1-SNAPSHOT"));
     }
 }
