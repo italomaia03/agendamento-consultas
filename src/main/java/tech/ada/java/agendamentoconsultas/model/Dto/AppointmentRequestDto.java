1e9bf73df45b5a1ea227eb6cedb386a8e69fd917
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-25 18:13:38 -0300

    feat: adicionar campo de validação para a descrição

--- src/main/java/tech/ada/java/agendamentoconsultas/model/Dto/AppointmentRequestDto.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Dto/AppointmentRequestDto.java
@@ -2,6 +2,7 @@ package tech.ada.java.agendamentoconsultas.model.Dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,5 +23,6 @@ public class AppointmentRequestDto implements Serializable {
     @FutureOrPresent(message = "Informe um horário válido")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime appointmentStartTime;
+    @NotBlank(message = "Informe uma descrição para a consulta")
     private String appointmentDescription;
 }
