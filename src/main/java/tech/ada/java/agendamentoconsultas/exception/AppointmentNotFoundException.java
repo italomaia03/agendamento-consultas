9ecc7ce9749dd0b396972648bc7a044405bc6472
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-25 23:57:12 -0300

    chore: retornar mensagem mais coerente com o contexto atual

--- src/main/java/tech/ada/java/agendamentoconsultas/exception/AppointmentNotFoundException.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/exception/AppointmentNotFoundException.java
@@ -2,6 +2,6 @@ package tech.ada.java.agendamentoconsultas.exception;
 
 public class AppointmentNotFoundException extends NotFoundException{
     public AppointmentNotFoundException() {
-        super("Não foi encontrado agendamento para o paciente ou médico informados");
+        super("O agendamento de consulta solicitado não foi encontrado");
     }
 }
