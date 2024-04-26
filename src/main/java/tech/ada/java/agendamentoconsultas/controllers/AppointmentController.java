a8885c1ec59b912314c75fe5e93abcd626b910c3
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-25 21:40:41 -0300

    feat: implementar rota de exclusão de Appointments

--- src/main/java/tech/ada/java/agendamentoconsultas/controllers/AppointmentController.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/controllers/AppointmentController.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tech.ada.java.agendamentoconsultas.model.Dto.AppointmentRequestDto;
 import tech.ada.java.agendamentoconsultas.model.Dto.AppointmentResponseDto;
-import tech.ada.java.agendamentoconsultas.model.Dto.AppointmentUpdateRequestDto;
+import tech.ada.java.agendamentoconsultas.model.Dto.AppointmentDeleteRequestDto;
 import tech.ada.java.agendamentoconsultas.service.AppointmentService;
 
 import java.time.LocalDate;
@@ -39,8 +39,12 @@ public class AppointmentController {
     }
 
     @PutMapping("/patients/{patientUuid}/doctors/{doctorUuid}")
-    public void update(@RequestBody AppointmentUpdateRequestDto request, @PathVariable UUID patientUuid, @PathVariable UUID doctorUuid) {
+    public void update(@RequestBody @Valid AppointmentRequestDto request, @PathVariable UUID patientUuid, @PathVariable UUID doctorUuid) {
         appointmentService.update(request, patientUuid, doctorUuid);
     }
 
+    @DeleteMapping("/doctors/{doctorUuid}/patients/{patientUuid}")
+    public void update(@RequestBody @Valid AppointmentDeleteRequestDto request, @PathVariable UUID patientUuid, @PathVariable UUID doctorUuid) {
+        appointmentService.delete(request, patientUuid, doctorUuid);
+    }
 }
