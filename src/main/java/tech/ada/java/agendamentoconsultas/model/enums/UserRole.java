55093ebc4f47301ee0ecc85ccc21001cedfd6d11
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-27 16:33:07 -0300

    feat: adicionar role ADMIN

--- src/main/java/tech/ada/java/agendamentoconsultas/model/enums/UserRole.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/enums/UserRole.java
@@ -1,16 +1,5 @@
 package tech.ada.java.agendamentoconsultas.model.enums;
 
 public enum UserRole {
-    PATIENT("patient"), DOCTOR("doctor");
-
-    private String role;
-
-    UserRole(String role){
-        this.role = role;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
+    PATIENT, DOCTOR, ADMIN;
 }
