772c80fabd46fd4eab79d26fdc4d11bda417334e
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-27 16:34:00 -0300

    feat: implementar interface UserDetails

--- src/main/java/tech/ada/java/agendamentoconsultas/model/Doctor.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Doctor.java
@@ -7,7 +7,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import tech.ada.java.agendamentoconsultas.model.enums.UserRole;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -16,19 +22,58 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @SQLRestriction("is_active = true")
-public class Doctor {
+public class Doctor implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String crm;
+    private String email;
+    private String password;
     private Boolean isActive = true;
     private String specialty;
     private UUID uuid = UUID.randomUUID();
+
+    private Boolean accountExpired = false;
+    private Boolean credentialsExpired = false;
+    private Boolean accountLocked = false;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.DOCTOR;
 
     @ManyToOne
     @JoinColumn(name = "address_id")
     @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private Address address;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
+    }
+
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !this.credentialsExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isActive;
+    }
 }
