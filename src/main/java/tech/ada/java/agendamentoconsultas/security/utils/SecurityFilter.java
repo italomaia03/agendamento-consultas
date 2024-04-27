7cf80c4dbd2abd545e7481a926b295cc35233646
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-27 20:38:30 -0300

    feat: implementar novo serviço de tokens baseado nas novas dependências

--- src/main/java/tech/ada/java/agendamentoconsultas/security/utils/SecurityFilter.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/security/utils/SecurityFilter.java
@@ -1,47 +1,57 @@
 package tech.ada.java.agendamentoconsultas.security.utils;
 
-import java.io.IOException;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import tech.ada.java.agendamentoconsultas.repository.PatientRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 import tech.ada.java.agendamentoconsultas.security.service.TokenService;
 
+import java.io.IOException;
+
+@Log4j2
 @Component
 @RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
-    private final PatientRepository patientRepository;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-                var token = this.recoverToken(request);
-                if(token != null){
-                    var login = this.tokenService.validateToken(token);
-                    UserDetails patient = patientRepository.findByEmail(login);
-
-                    var authentication = new UsernamePasswordAuthenticationToken(patient, null, patient.getAuthorities());
-                    
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
-                filterChain.doFilter(request, response);
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String token = authHeader.substring(7);
+        authenticateUserFromToken(request, token);
+        filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request) {
-        var autHeader = request.getHeader("Authorization");
-        if(autHeader == null) return null;
-        return autHeader.replace("Bearer ", "");
+    private void authenticateUserFromToken(HttpServletRequest request, String token) {
+        final String username = this.tokenService.extractUsername(token);
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (tokenService.isTokenValid(token, userDetails)) {
+                setAuthTokenOnSecurityContext(request, userDetails);
+            }
+        }
     }
-    
+
+    private void setAuthTokenOnSecurityContext(HttpServletRequest request, UserDetails userDetails) {
+        var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
 }
