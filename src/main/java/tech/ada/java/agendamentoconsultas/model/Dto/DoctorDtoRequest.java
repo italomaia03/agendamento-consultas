58dbbd08440bca55b1ed81be32a49f3fe52f3434
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-27 16:35:37 -0300

    feat: adicionar novos campos ao DTO

--- src/main/java/tech/ada/java/agendamentoconsultas/model/Dto/DoctorDtoRequest.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/model/Dto/DoctorDtoRequest.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DoctorDtoRequest {
     private String name;
+    private String email;
+    private String password;
     private String crm;
     private String specialty;
     private AddressRequestDto address;
