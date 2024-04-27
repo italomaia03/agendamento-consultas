f99e319d8074f8a236329f85cd39f6e3aeb073f7
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-27 20:35:56 -0300

    fix: fazer cast de UserDetails

--- src/main/java/tech/ada/java/agendamentoconsultas/service/impl/AuthImpl.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/service/impl/AuthImpl.java
@@ -1,12 +1,10 @@
 package tech.ada.java.agendamentoconsultas.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
-import lombok.RequiredArgsConstructor;
-import tech.ada.java.agendamentoconsultas.model.Patient;
 import tech.ada.java.agendamentoconsultas.model.Dto.LoginRequestDto;
 import tech.ada.java.agendamentoconsultas.model.Dto.LoginResponseDto;
 import tech.ada.java.agendamentoconsultas.security.service.TokenService;
@@ -14,23 +12,23 @@ import tech.ada.java.agendamentoconsultas.service.AuthService;
 
 @Service
 @RequiredArgsConstructor
-public class AuthImpl implements AuthService{
+public class AuthImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
-    
+
+
     @Override
     public LoginResponseDto login(LoginRequestDto request) {
-        var usenamePassword = new UsernamePasswordAuthenticationToken(request.email(),request.senha());
+        var usenamePassword = new UsernamePasswordAuthenticationToken(request.email(), request.senha());
         var auth = this.authenticationManager.authenticate(usenamePassword);
-
-        var token = tokenService.generatedToken((Patient) auth.getPrincipal());
+        var token = tokenService.generatedToken((UserDetails) auth.getPrincipal());
         return new LoginResponseDto(token);
     }
 
     @Override
     public void logout() {
-        
+
     }
-    
+
 }
