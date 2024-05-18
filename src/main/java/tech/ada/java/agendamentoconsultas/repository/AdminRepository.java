b1470300d45086491559834d64cc97f87aaba911
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-05-18 17:57:15 -0300

    fix: adicionar anotação de repositório

--- src/main/java/tech/ada/java/agendamentoconsultas/repository/AdminRepository.java
+++ src/main/java/tech/ada/java/agendamentoconsultas/repository/AdminRepository.java
@@ -1,10 +1,12 @@
 package tech.ada.java.agendamentoconsultas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.ada.java.agendamentoconsultas.model.Admin;
 
 import java.util.Optional;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByEmail(String email);
 }
